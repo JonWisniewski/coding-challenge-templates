90482a4a1161025299529faa0bed9146d43da979
JonWisniewski <jwisnie5@kent.edu>
2021-11-06 18:40:55 -0400

    Added Hello World Back

--- spring-template/src/main/java/io/lightfeather/springtemplate/bootloader/Application.java
+++ spring-template/src/main/java/io/lightfeather/springtemplate/bootloader/Application.java
@@ -3,11 +3,17 @@ package io.lightfeather.springtemplate.bootloader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class Application {
 
+  @RequestMapping("/")
+  public String home() {
+	  return "Hello World";
+  }
+		
   @Bean
   public WebClient.Builder getWebClientBuilder() {
 	 return WebClient.builder();
